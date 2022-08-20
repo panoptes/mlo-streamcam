2f6ac96c1650698e5fceb4eee8b3165c5f2e0209
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-08-19 17:51:05 -1000

    Use more env var. Default to 4k instead of uhd216

--- start-stream.sh
+++ start-stream.sh
@@ -2,7 +2,13 @@
 
 # Source .env file in directory.
 set -o allexport; source .env; set +o allexport
+
 STREAM_KEY="${STREAM_KEY:-ENTER STREAM KEY}"
+FRAMERATE=${FRAMERATE:-30}
+KEYFRAMERATE=${KEYFRAMERATE:-30}  # in fps
+VIDEO_SIZE=${VIDEO_SIZE:-4k}
+BUFFER_SIZE=${BUFFER_SIZE:-4M}
+MAX_RATE=${MAX_RATE:-20M}
 
 # Clears video buffer message.
 modprobe -v -r uvcvideo && modprobe -v uvcvideo
@@ -14,16 +20,16 @@ ffmpeg \
     -f lavfi -i anullsrc -acodec aac \
     -f v4l2 \
     -thread_queue_size 128 \
-    -framerate 30 \
-    -video_size 3840x2160 \
+    -framerate "${FRAMERATE}" \
+    -video_size "${VIDEO_SIZE}" \
     -i /dev/video0 \
     -vf "drawtext=textfile=time.txt:reload=1:fontcolor=yellow:fontsize=36:box=1:boxcolor=black@0.5:boxborderw=5:x=w-text_w-10:y=(h-text_h)-10,drawtext=textfile=banner.txt:reload=1:fontcolor=yellow:fontsize=36:box=1:boxcolor=black@0.5:boxborderw=5:x=10:y=(h-text_h)-10" \
     -vcodec libx264 \
     -pix_fmt yuv422p \
-    -s 3840x2160 \
+    -s "${VIDEO_SIZE}" \
     -preset ultrafast \
-    -r 30 \
-    -g 30 \
-    -bufsize 4M \
-    -maxrate 25M \
+    -r "${FRAMERATE}" \
+    -g "${KEYFRAMERATE}" \
+    -bufsize "${BUFFER_SIZE}" \
+    -maxrate "${MAX_RATE}" \
     -f flv "rtmp://a.rtmp.youtube.com/live2/${STREAM_KEY}"
