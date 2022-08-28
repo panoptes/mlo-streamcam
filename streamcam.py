f53a57f0a9aba7559484771c0e8ff0e1c97765c7
Wilfred Tyler Gee <wtylergee@gmail.com>
2022-08-27 17:56:22 -1000

    Updating python support

--- streamcam.py
+++ streamcam.py
@@ -7,11 +7,49 @@ load_dotenv()
 
 stream_key = os.environ['STREAM_KEY']
 stream_url = f'rtmp://a.rtmp.youtube.com/live2/{stream_key}'
+
+crf=os.getenv('CRF', 20)
+preset=os.getenv('PRESET', 'ultrafast')
+framerate=os.getenv('FRAMERATE', 30)
+keyframerate=os.getenv('KEYFRAMERATE', 30)
+# n.b. ffmpeg-python does not support named sized, e.g. uhd2160.
+video_size=os.getenv('VIDEO_SIZE', '3840x2160')
+bufsize=os.getenv('BUFSIZE', '5M')
+maxrate=os.getenv('MAXRATE', '40M')
+
 print(f'Starting stream on {stream_url}')
 
-video_in = (
-    ffmpeg.input('/dev/video0', framerate=24, video_size='uhd2160')
-    .filter('drawtext', text='Project PANOPTES MLO Streamcam', x='w-text_w-10', y='h-text_h-10', color='yellow', font='mono')
-    .output(stream_url, format='flv')
-    .run()
+font_styles = dict(
+    fontcolor='yellow',
+    fontsize=36,
+    box=1,
+    boxcolor='black@0.5',
+    boxborderw=5,
 )
+
+# Set up the video source and a fake audio source.
+video_in = ffmpeg.input('/dev/video0', s='3840x2160', framerate=30, thread_queue_size=512)
+audio_in = ffmpeg.input('anullsrc', format='lavfi')
+
+# Filter the frames with a simple two-frame time-blend.
+video_in = video_in.filter('tblend', all_mode='average')
+
+# Add the text from the banner and the time.
+video_in = video_in.drawtext(textfile='banner.txt', reload=True, **font_styles, y='h-10-text_h')
+video_in = video_in.drawtext(textfile='time.txt', reload=True, **font_styles, x='w-10-text_w', y='h-10-text_h')
+
+output = ffmpeg.output(audio_in, 
+                        video_in, 
+                        stream_url, 
+                        format='flv', 
+                        vcodec='libx264', 
+                        crf=crf, 
+                        preset=preset, 
+                        r=framerate, 
+                        g=keyframerate, 
+                        s=video_size, 
+                        bufsize=bufsize, 
+                        maxrate=maxrate
+                        )
+
+output.run()
