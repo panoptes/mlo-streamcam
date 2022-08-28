d6c89085a13d10b431201e2da0aabc917429bc1c
Wilfred Gee <wtylergee@gmail.com>
2022-08-27 18:26:08 -1000

    Using pydantic for the settings.

--- streamcam.py
+++ streamcam.py
@@ -1,23 +1,27 @@
-import os
 import ffmpeg
-from datetime import datetime as dt
-from dotenv import load_dotenv
+from pydantic import BaseSettings
 
-load_dotenv()
 
-stream_key = os.environ['STREAM_KEY']
-stream_url = f'rtmp://a.rtmp.youtube.com/live2/{stream_key}'
+class VideoSettings(BaseSettings):
+    """Settings for youtube streaming."""
+    stream_key: str
+    crf: int = 20
+    preset: str = 'ultrafast'
+    framerate: int = 30
+    keyframerate: int = 60
+    # n.b. ffmpeg-python does not support named sized, e.g. uhd2160.
+    video_size: str = '3840x2160'
+    bufsize: str | int = '5M'
+    maxrate: str | int = '40M'
+    thread_queue_size: int = 512
 
-crf=os.getenv('CRF', 20)
-preset=os.getenv('PRESET', 'ultrafast')
-framerate=os.getenv('FRAMERATE', 30)
-keyframerate=os.getenv('KEYFRAMERATE', 30)
-# n.b. ffmpeg-python does not support named sized, e.g. uhd2160.
-video_size=os.getenv('VIDEO_SIZE', '3840x2160')
-bufsize=os.getenv('BUFSIZE', '5M')
-maxrate=os.getenv('MAXRATE', '40M')
+    @property
+    def stream_url(self):
+        return f'rtmp://a.rtmp.youtube.com/live2/{self.stream_key}'
 
-print(f'Starting stream on {stream_url}')
+    class Config:
+        env_prefix = 'pocs_'
+
 
 font_styles = dict(
     fontcolor='yellow',
@@ -27,29 +31,40 @@ font_styles = dict(
     boxborderw=5,
 )
 
+video_settings = VideoSettings()
+
 # Set up the video source and a fake audio source.
-video_in = ffmpeg.input('/dev/video0', s='3840x2160', framerate=30, thread_queue_size=512)
+video_in = ffmpeg.input('/dev/video0', s=video_settings.video_size,
+                        framerate=video_settings.framerate,
+                        thread_queue_size=video_settings.thread_queue_size)
 audio_in = ffmpeg.input('anullsrc', format='lavfi')
 
 # Filter the frames with a simple two-frame time-blend.
 video_in = video_in.filter('tblend', all_mode='average')
 
 # Add the text from the banner and the time.
-video_in = video_in.drawtext(textfile='banner.txt', reload=True, **font_styles, y='h-10-text_h')
-video_in = video_in.drawtext(textfile='time.txt', reload=True, **font_styles, x='w-10-text_w', y='h-10-text_h')
+video_in = video_in.drawtext(textfile='banner.txt',
+                             reload=True,
+                             **font_styles,
+                             y='h-10-text_h')
+video_in = video_in.drawtext(textfile='time.txt',
+                             reload=True,
+                             **font_styles,
+                             x='w-10-text_w',
+                             y='h-10-text_h')
 
-output = ffmpeg.output(audio_in, 
-                        video_in, 
-                        stream_url, 
-                        format='flv', 
-                        vcodec='libx264', 
-                        crf=crf, 
-                        preset=preset, 
-                        r=framerate, 
-                        g=keyframerate, 
-                        s=video_size, 
-                        bufsize=bufsize, 
-                        maxrate=maxrate
-                        )
+output = ffmpeg.output(audio_in,
+                       video_in,
+                       video_settings.stream_url,
+                       format='flv',
+                       vcodec='libx264',
+                       crf=video_settings.crf,
+                       preset=video_settings.preset,
+                       r=video_settings.framerate,
+                       g=video_settings.keyframerate,
+                       s=video_settings.video_size,
+                       bufsize=video_settings.bufsize,
+                       maxrate=video_settings.maxrate
+                       )
 
 output.run()
